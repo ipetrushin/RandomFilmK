4dd603e399a0913af36ad9db11b9f5ed7389c974
Ivan Petr <ivan.kiel@gmail.com>
2021-10-08 12:51:35 +0800

    Layout and resources

--- app/src/main/java/com/example/randomfilmk/MainActivity.kt
+++ app/src/main/java/com/example/randomfilmk/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.randomfilmk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val movies = resources.getStringArray(R.array.movies)
+        Log.d("mytag", movies.toString())
     }
 }
